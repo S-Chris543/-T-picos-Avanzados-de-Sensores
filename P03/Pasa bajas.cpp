25d94beb0f490cdb581a5710be65107425938900
CHRISRB62 <127573334+S-Chris543@users.noreply.github.com>
2026-04-13 02:15:19 -0600

    Update Pasa bajas.cpp

--- P03/Pasa bajas.cpp
+++ P03/Pasa bajas.cpp
@@ -7,8 +7,10 @@ Adafruit_MPU6050 mpu;
 float pitch = 0;
 float roll = 0;
 
-unsigned long tiempoPrevio;
-float dt;
+float pitch_f = 0;
+float roll_f = 0;
+
+float alpha = 0.2;   // coeficiente del filtro
 
 void setup() {
 
@@ -16,13 +18,11 @@ void setup() {
   Wire.begin();
 
   if (!mpu.begin()) {
-    Serial.println("No se encontró MPU6050");
+    Serial.println("MPU6050 no encontrado");
     while (1);
   }
 
   Serial.println("MPU6050 listo");
-
-  tiempoPrevio = millis();
 }
 
 void loop() {
@@ -30,20 +30,26 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  unsigned long tiempoActual = millis();
-  dt = (tiempoActual - tiempoPrevio) / 1000.0;
-  tiempoPrevio = tiempoActual;
+  float Ax = a.acceleration.x;
+  float Ay = a.acceleration.y;
+  float Az = a.acceleration.z;
 
-  float gyroX = g.gyro.x * 180 / PI; // convertir a grados/s
-  float gyroY = g.gyro.y * 180 / PI;
+  // Calcular ángulos
+  pitch = atan2(-Ax, sqrt(Ay*Ay + Az*Az)) * 180 / PI;
+  roll  = atan2(Ay, Az) * 180 / PI;
 
-  roll  += gyroX * dt;
-  pitch += gyroY * dt;
+  // Filtro pasa bajas
+  pitch_f = alpha * pitch + (1 - alpha) * pitch_f;
+  roll_f  = alpha * roll  + (1 - alpha) * roll_f;
 
-  Serial.print("Roll: ");
-  Serial.print(roll);
-  Serial.print("  Pitch: ");
-  Serial.println(pitch);
+  Serial.print("Pitch: ");
+  Serial.print(pitch_f);
+
+  Serial.print("  Roll: ");
+  Serial.println(roll_f);
+
+  delay(20);
+}
 
   delay(10);
 }
